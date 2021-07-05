2c8ea4d08e83f8f693222af99fc7f9d73604305b
Christopher Woggon <christopher.woggon@gmail.com>
2021-07-05 18:42:09 +0200

    Implemented get_external_ip template function

--- functions/group-member/set-address/handler.py
+++ functions/group-member/set-address/handler.py
@@ -2,22 +2,37 @@ import requests
 import json
 import os
 import sys
+from member_template import get_external_ip
 
-ip = "127.0.0.1"
 port = "5000"
 
 
 def handle(req):
+    try:
+        ip = get_external_ip()
+    except:
+        return json.dumps({
+            "status-code": 500,
+            "message": "Error while retrieving external IP from environment variable in group member. Make sure it was set during function deployment."
+        })
+
     payload = json.loads(req)
 
     function_name = payload["function_name"]
     address = payload["address"]
 
-    response = requests.post(f"http://{ip}:{port}/set-address", data=f"{function_name},{address}")
+    try:
+        response = requests.post(f"http://{ip}:{port}/set-address", data=f"{function_name},{address}")
+    except:
+        return json.dumps({
+            "status-code": 500,
+            "message": f"Node at {ip}:{port} could not be reached."
+        })
+
     if response.status_code != 200:
         return json.dumps({
             "status-code": response.status_code,
-            "message": "Address could not be set. Please try again later."
+            "message": f"Address {address} for function {function_name} could not be set on node at {ip}:{port}. Please try again later."
         })
 
     return json.dumps({
---
+++ functions/group-member/set-address/member_template.py
@@ -0,0 +1,7 @@
+# V1.0 2021.07.05 18:05
+
+import os
+
+
+def get_external_ip():
+    return os.environ['externalIp']
