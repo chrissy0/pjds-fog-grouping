159aa5da3942550b08cf12f7d7df5fa0369bb26f
BieVic <victorbieszka@gmail.com>
2021-07-11 19:53:58 +0200

    ADD: node registration

--- load-scanner/load_scanner.py
+++ load-scanner/load_scanner.py
@@ -6,16 +6,29 @@ SQLITE_PORT = 5000
 
 
 def get_leader_address():
-    res = requests.get(f"http://localhost:{SQLITE_PORT}/get-leader-address")
-    return res.text
+    req = requests.get(f"http://localhost:{SQLITE_PORT}/get-leader-address")
+    return req.text
+
+
+def compute_cpu():
+    return psutil.getloadavg()[0] / psutil.cpu_count() * 100
+
+
+def compute_memory():
+    mem = psutil.virtual_memory()
+    return round(1 - mem.available / mem.total, 2)
 
 
 if __name__ == '__main__':
-    ip = requests.get('https://api.ipify.org').text
-    while True:
-        leader = get_leader_address()
-        cpu_usage = psutil.getloadavg()[0] / psutil.cpu_count() * 100
-        mem = psutil.virtual_memory()
-        memory_usage = round(1 - mem.available / mem.total, 2)
-        res = requests.post(f"http://{leader}:{SQLITE_PORT}/update-node", data=f"{ip},{cpu_usage},{memory_usage}")
-        time.sleep(10)
+    leader = get_leader_address()
+    if len(leader) != 0:
+        ip = requests.get('https://api.ipify.org').text
+        f = open("/var/lib/faasd/secrets/basic-auth-password", "r")
+        faasd_secret = f.read()
+        requests.post(f"http://{leader}:{SQLITE_PORT}/add-node",
+                      data=f"{ip},{compute_cpu()},{compute_memory()},{faasd_secret}")
+        f.close()
+        while True:
+            time.sleep(10)
+            leader = get_leader_address()
+            r = requests.post(f"http://{leader}:{SQLITE_PORT}/update-node", data=f"{ip},{compute_cpu()},{compute_memory()}")
