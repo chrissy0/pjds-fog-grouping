630fe99adc54110415b7bf417572f631a4dd9360
Berit Frech <berit.frech@campus.tu-berlin.de>
2021-07-13 10:26:43 +0200

    add load analysis + workflow extension + nested JSON as Input

--- functions/leader/deploy-workflow/handler.py
+++ functions/leader/deploy-workflow/handler.py
@@ -6,11 +6,13 @@ from member_utility import get_external_ip
 
 port = "5000"
 
-
 def handle(req):
     body = json.loads(req)
-    workflow = body["workflow"]
-    functions = body["functions"]
+
+    try:
+        replicas = int(body["replicas"])
+    except:
+        replicas = 2
 
     try:
         external_ip = get_external_ip()
@@ -28,85 +30,106 @@ def handle(req):
             "message": f"There was an error while retrieving group members for the leader with {external_ip}"
         })
 
+
+    # Load Analysis, get nodes ordered by available CPU/RAM
     nodes = res.text.split(",")
+    nodes = [node.split() for node in nodes]
+    nodes = sorted(nodes, key=lambda x: (x[1], x[2]))
 
-    # Turning "a->b->c" into [("a", "b"), ("b", "c"), ("c", None)]. The last element is needed so "c" is still deployed.
-    function_dependencies = []
-    functions_chain = workflow.split("->")
-    for i in range(len(functions_chain) - 1):
-        function_dependencies.append((functions_chain[i], functions_chain[i+1]))
-    function_dependencies.append((functions_chain[-1], None))
+    # TODO: leave out nodes below certain threshold?
 
-    # TODO: Insert Load Analysis, get nodes ordered by available CPU/RAM
-
-    # TODO: workflow
-
-
-    for i, (source_func, target_func) in enumerate(function_dependencies):
-        # Modulo % so we don't exceed number of nodes. Wraps around!
-        source_node = nodes[i % len(nodes)]
-        source_node_ip = source_node.split()[0]
-        source_node_secret = source_node.split()[1]
-        # Modulo % so we don't exceed number of nodes. Wraps around!
-        target_node = nodes[(i+1) % len(nodes)]
-        target_node_ip = target_node.split()[0]
-
-        node_address = f"http://admin:{source_node_secret}@{source_node_ip}:8080"
-        url = node_address + "/system/functions"
-
-        headers = {
-            'Content-Type': 'application/json'
-        }
-
-        # First delete function if already exists
-        payload = json.dumps({
-            "functionName": source_func
-        })
-        response = requests.request("DELETE", url, headers=headers, data=payload)
-        if response.status_code != 200 and response.status_code != 404:
-            return json.dumps({
-                "status-code": res.status_code,
-                "message": f"Problem with deletion of {func}",
-                "error-message": response.text
-            })
-
-        # Deploy Function
-        source_func_rep = functions[source_func]
-        payload = json.dumps({
-            "service": source_func,
-            "network": "func_functions",
-            "image": source_func_rep,
-            "readOnlyRootFilesystem": True,
-            "envVars": {
-                "externalIp": source_node_ip
-            }
-        })
-
-        response = requests.request("POST", url, headers=headers, data=payload)
-        if response.status_code != 200:
-            return json.dumps({
-                "status-code": res.status_code,
-                "message": f"Could not deploy function {source_func}",
-                "error-message": response.text
-            })
-
-        # Store Function in SQLite
-        res = requests.post(f"http://{external_ip}:{port}/add-fn", data=f"{source_func},{source_node_ip}")
-        if res.status_code != 200:
-            return json.dumps({
-                "status-code": res.status_code,
-                "message": f"Could not store function {source_func} in database"
-            })
-
-        # Save information about next function:
-        if target_func is not None:
-            res = requests.post(f"http://{source_node_ip}:{port}/set-address", data=f"{target_func},{target_node_ip}")
-            if res.status_code != 200:
-                return json.dumps({
-                    "status-code": res.status_code,
-                    "message": f"Address {target_node_ip} for function {target_func} could not be set on node at {source_node_ip}:{port}."
-                })
+    for i in range(replicas):
+        deploy_function(nodes, body, external_ip, i)
 
 
     return "Deployed new workflow"
 
+
+def deploy_function(nodes, body, external_ip, i, previous=None):
+    try:
+        name = body["name"]
+    except:
+        return json.dumps({
+            "message": "Please state the name of your function"
+        })
+
+    try:
+        registry = body["registry"]
+    except:
+        return json.dumps({
+            "message": "Please state the link to the registry of your function"
+        })
+
+    try:
+        calls = body["calls"]
+    except:
+        calls = None
+
+    node = nodes[ i % len(nodes) ]
+    node_ip = node[0]
+    node_secret = node[3]
+
+    # Save Information about function on previous node
+    if previous is not None:
+        res = requests.post(f"http://{previous}:{port}/set-address", data=f"{name},{node_ip}")
+        if res.status_code != 200:
+            return json.dumps({
+                "status-code": res.status_code,
+                "message": f"Address {node_ip} for function {name} could not be set on node at {previous}:{port}."
+            })
+
+
+    node_address = f"http://admin:{node_secret}@{node_ip}:8080"
+    url = node_address + "/system/functions"
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    # First delete function if already exists
+    payload = json.dumps({
+        "functionName": name
+    })
+    response = requests.request("DELETE", url, headers=headers, data=payload)
+    if response.status_code != 200 and response.status_code != 404:
+        return json.dumps({
+            "status-code": res.status_code,
+            "message": f"Problem with deletion of {name}",
+            "error-message": response.text
+        })
+
+    # Deploy Function
+    payload = json.dumps({
+        "service": name,
+        "network": "func_functions",
+        "image": registry,
+        "readOnlyRootFilesystem": True,
+        "envVars": {
+            "externalIp": node_ip
+        }
+    })
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    if response.status_code != 200:
+        return json.dumps({
+            "status-code": response.status_code,
+            "message": f"Could not deploy function {name}",
+            "error-message": response.text
+        })
+
+    # Store Function in SQLite
+    res = requests.post(f"http://{external_ip}:{port}/add-fn", data=f"{name},{node_ip}")
+    if res.status_code != 200:
+        return json.dumps({
+            "status-code": res.status_code,
+            "message": f"Could not store function {name} in database"
+        })
+
+    if calls is not None:
+        for call in calls:
+            i += 1
+            deploy_function(nodes, call, external_ip, i, node_ip)
+    else:
+        return "Calls is empty"
+
+    return "Finished Internal Call"
