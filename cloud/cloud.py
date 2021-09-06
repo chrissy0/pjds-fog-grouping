ce284204028f94db3dc1eebe6e721f71c332dbf5
Christopher Woggon <christopher.woggon@gmail.com>
2021-09-06 13:10:31 +0200

    Cloud can now suggest clusters close to the cluster that sent the request

--- cloud/cloud.py
+++ cloud/cloud.py
@@ -1,10 +1,32 @@
+from math import sin, cos, sqrt, atan2, radians
+
 from flask import Flask
+from flask import jsonify
 from flask import request
 
 app = Flask(__name__)
 
-
 clusters = {}
+
+
+def calculate_distance(location1, location2):
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(float(location1["lat"]))
+    lon1 = radians(float(location1["lon"]))
+    lat2 = radians(float(location2["lat"]))
+    lon2 = radians(float(location2["lon"]))
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
 
 
 def response_object(message=None, code=200):
@@ -19,7 +41,7 @@ def register_cluster():
     ip = request.form["ip"]
     port = request.form["port"]
     location = {
-        "long": request.form["long"],
+        "lon": request.form["lon"],
         "lat": request.form["lat"]
     }
     # Overwrites old entries
@@ -40,3 +62,24 @@ def deregister_cluster():
     except KeyError:
         return response_object(f"Cluster ip '{ip}' is already non-existent and cannot be deleted.", 409)
 
+
+@app.route('/cluster-suggestions', methods=['POST'])
+def cluster_suggestions():
+    request_source_ip = request.form["ip"]
+    max_number_of_requested_clusters = int(request.form["limit"])
+
+    if request_source_ip not in clusters:
+        return response_object(f"Source ip is not known, thus surrounding clusters cannot be suggested.", 409)
+
+    source_location = clusters[request_source_ip]["location"]
+
+    clusters_incl_distance = list(map(lambda cluster: {
+        "ip": cluster,
+        "port": clusters[cluster]["port"],
+        "distance": calculate_distance(source_location, clusters[cluster]["location"])
+    }, clusters))
+    clusters_incl_distance = sorted(clusters_incl_distance, key=lambda k: k['distance'])
+
+    return jsonify({
+        "closest_clusters": clusters_incl_distance[:max_number_of_requested_clusters]
+    })
