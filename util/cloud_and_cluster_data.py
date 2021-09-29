a74fbe535b3fc73ea40b6198aca43a3a84a3b458
Christopher Woggon <christopher.woggon@gmail.com>
2021-09-29 13:37:19 +0200

    Added placeholders

--- util/cloud_and_cluster_data.py
+++ util/cloud_and_cluster_data.py
@@ -3,29 +3,28 @@ cloud = {
 }
 
 
-# TODO change to placeholder
 cluster_list = [
     {
-        "name": "pjds-cluster-1",
-        "openfaas_ip": "35.246.29.50",  # kubectl get -n openfaas svc/gateway-external
-        "kubectl_pod_ip": "10.3.242.117",  # kubectl get service kubectl-gcloud-service --output yaml | grep clusterIP:
-        "cluster_ip": "35.189.80.227",  # kubectl get svc
-        "secret": "F5sV631qQJCSeKSTY22oD5840",  # echo $(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
-        "lat": "52.3454",
-        "lon": "13.645",
-        "cluster_name": "pjds-cluster-1",
-        "zone": "europe-west2-c",
+        "name": "<placeholder>",
+        "openfaas_ip": "<placeholder>",  # kubectl get -n openfaas svc/gateway-external
+        "kubectl_pod_ip": "<placeholder>",  # kubectl get service kubectl-gcloud-service --output yaml | grep clusterIP:
+        "cluster_ip": "<placeholder>",  # kubectl get svc
+        "secret": "<placeholder>",  # echo $(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
+        "lat": "<placeholder>",
+        "lon": "<placeholder>",
+        "cluster_name": "<placeholder>",
+        "zone": "<placeholder>",
     },
     {
-        "name": "pjds-cluster-2",
-        "openfaas_ip": "34.105.201.225",
-        "kubectl_pod_ip": "10.115.241.37",
-        "cluster_ip": "34.142.80.184",
-        "secret": "Hf9k493FIaBp40O97yyo2Xj7k",
-        "lat": "40.32444223",
-        "lon": "16.87498",
-        "cluster_name": "pjds-cluster-2",
-        "zone": "europe-west2-c",
+        "name": "<placeholder>",
+        "openfaas_ip": "<placeholder>",  # kubectl get -n openfaas svc/gateway-external
+        "kubectl_pod_ip": "<placeholder>",  # kubectl get service kubectl-gcloud-service --output yaml | grep clusterIP:
+        "cluster_ip": "<placeholder>",  # kubectl get svc
+        "secret": "<placeholder>",  # echo $(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
+        "lat": "<placeholder>",
+        "lon": "<placeholder>",
+        "cluster_name": "<placeholder>",
+        "zone": "<placeholder>",
     },
 ]
 
